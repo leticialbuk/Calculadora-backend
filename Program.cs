08c551e9e6c0cb0f3b0db1e0a9c294eb41cff06a
leticialbuk <leticia.albuk@gmail.com>
2023-03-14 17:18:25 -0300

    soma e subtracao

--- Program.cs
+++ Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Subtracao();
+    }
+    static void Soma()
+
+    {
+        Console.Clear();
+
+        Console.WriteLine("Primeiro valor: ");
+        float v1 = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Segundo valor: ");
+        float v2 = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("");
+
+        float resultado = v1 + v2;
+        Console.WriteLine($"O resultado da soma é {resultado}");
+        Console.ReadKey();
+    }
+
+    static void Subtracao()
+    {
+        Console.Clear();
+
+        Console.WriteLine("Primeiro valor: ");
+        float v1 = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Segundo valor: ");
+        float v2 = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("");
+
+
+        float resultado = v1 - v2;
+        Console.WriteLine($"O resultado da subtração é {resultado}");
+        Console.ReadKey();
+    }
+}
